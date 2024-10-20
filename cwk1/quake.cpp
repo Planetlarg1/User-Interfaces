a20f207c44938ab1f24b82b9013e79b0aa43fbd0
Planetlarg1 <146741451+Planetlarg1@users.noreply.github.com>
2024-10-20 21:07:39 +0000

    S1

--- cwk1/quake.cpp
+++ cwk1/quake.cpp
@@ -9,7 +9,22 @@ using namespace std;
 Quake::Quake(const string& tm, double lat, double lon, double dep, double mag):
   time(tm), latitude(lat), longitude(lon), depth(dep), magnitude(mag)
 {
-  // Add validation code here
+  // Check depth not negative
+  if (depth<0){
+    throw invalid_argument("Invalid argument: Depth cannot be negative");
+  }
+  // Chech magnitude not negative
+  if (magnitude<0){
+    throw invalid_argument("Invalid argument: Magnitute cannot be negative");
+  }
+  // Check latitude in valid range (-90 to 90)
+  if (latitude<-90 || latitude >90){
+    throw invalid_argument("Invalid argument: Latitude not within valid range");
+  }
+  //Check longitude in valid range (-180 to 180)
+  if (longitude<-180 || longitude >180){
+    throw invalid_argument("Invalid argument: Longitude not within valid range");
+  }
 }
 
 
